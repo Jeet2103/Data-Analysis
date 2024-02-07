37151741b7f4f3b3a83346fe3572efde55a52239
Jeet2103 <jeetnandigrami2003@gmail.com>
2024-02-07 21:44:23 +0530

    modified

--- X_power_n_iter.c
+++ X_power_n_iter.c
@@ -3,29 +3,35 @@
 // Function for O(n) Time Complexity
 long long compute_Xn_linear(int X, int n) {
     long long result = 1;
+    int comp=0;
     for (int i = 0; i < n; ++i) {
         result *= X;
+        comp++;
     }
+    printf("The Number of comparision is : \t%d\n",comp);
     return result;
 }
 
 // Function for O(log n) Time Complexity
 long long compute_Xn_logarithmic(int X, int n) {
     long long result = 1;
+    int comp=0;
     while (n > 0) {
         if (n % 2 == 1) {
             result *= X;
         }
         X *= X;
         n /= 2;
+        comp++;
     }
+    printf("The Number of comparision is : \t%d\n",comp);
     return result;
 }
 
 int main() {
     // Example usage:
-    int X = 10;
-    int n = 5;
+    int X = 2;
+    int n = 10;
 
     // O(n) time complexity
     long long result_linear = compute_Xn_linear(X, n);
